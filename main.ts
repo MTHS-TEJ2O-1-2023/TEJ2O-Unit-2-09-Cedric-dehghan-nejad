a7555226810df83fc4ee41a8735743d3ddc18c7f
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-10 16:47:00 -0400

    typescript change 2

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Cedric
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variable
+ let randomNumber: number = -1
+  randomNumber = -1
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function() {
+  randomNumber = randint(0, 2)
+  basic.clearScreen()
+})
+  // if randomNumber was 0 
+  if (randomNumber == 0) {
+  basic.showIcon(IconNames.Scissors)
+  }
+
+  // if randomNumber was 1 
+  
+if (randomNumber == 1) {
+basic.showLeds(`
+# # # # #
+# . . . #
+# . . . #
+# . . . #
+# # # # #
+`)
+}
+
+// if randomNumber was 2 
+
+if (randomNumber == 2) {
+basic.showLeds(`
+. . . . .
+. # # # .
+. # # # .
+. # # # .
+. . . . .
+`)
+
+  // pause and show you are ready again
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
+} 
