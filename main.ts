348c485682de743c58d0890e78dcc917a788589f
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-10 18:38:29 -0400

    ijij

--- main.ts
+++ main.ts
@@ -5,45 +5,56 @@
  * This program ...
 */
 
-// variable
-let randomNumber: number = -1
-randomNumber = -1
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+
+// variable
+let randomNumber: number = -1
+  let scoreboard: number = 0
+
+// increase scoreboard
+input.onButtonPressed(Button.A, function () {
+ scoreboard = scoreboard + 1
+ basic.showIcon(IconNames.Yes)
+})
+
+//show scoreboard
+input.onButtonPressed(Button.B, function () {
+basic.showNumber(scoreboard)
+})
 
 input.onGesture(Gesture.Shake, function () {
   randomNumber = randint(0, 2)
   basic.clearScreen()
-})
 // if randomNumber was 0
-if (randomNumber == 0) {
+  if (randomNumber == 0) {
   basic.showIcon(IconNames.Scissors)
-}
+ }
 
-// if randomNumber was 1 
+  // if randomNumber was 1 
 
-if (randomNumber === 1) {
+ if (randomNumber == 1) {
   basic.showLeds(`
+  . . . . .
+  . # # # .
+  . # # # .
+  . # # # .
+  . . . . .
+  `)
+ }
+
+ // if randomNumber was 2
+  if (randomNumber == 2) {
+basic.showLeds(`
 # # # # #
 # . . . #
 # . . . #
 # . . . #
 # # # # #
 `)
-}
-
-// if randomNumber was 2
-
-if (randomNumber === 2) {
-  basic.showLeds(`
-. . . . .
-. # # # .
-. # # # .
-. # # # .
-. . . . .
-`)
-
+  }
   // pause and show you are ready again
-  basic.pause(1000)
+  basic.pause(5000)
   basic.showIcon(IconNames.Happy)
-}
+})
