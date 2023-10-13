418e34e4cb991f8623fcb17910bb9dd6c5723876
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-12 22:37:41 -0400

    type

--- main.ts
+++ main.ts
@@ -19,6 +19,7 @@ input.onButtonPressed(Button.A, function () {
   basic.showIcon(IconNames.Yes)
 })
 
+//show scoreboard
 input.onButtonPressed(Button.B, function () {
   basic.showNumber(scoreboard)
 })
